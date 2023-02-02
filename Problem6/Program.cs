8e8069be0cfa87cb7e4299962cf52e2231ca2562
amira <amira@AMIRA>
2023-02-02 21:07:02 +0000

    Add project files.

---
+++ Problem6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the word or phrase:");
+            string
+                actualWords = Console.ReadLine();
+            string
+                reverseWords = string.Empty;
+            for (int i = actualWords.Length-1; i >= 0; i--)
+            {
+                reverseWords += actualWords[i];
+            }
+            Console.WriteLine(reverseWords);
+            if(actualWords==reverseWords) 
+                Console.WriteLine("Palindrome");
+                else Console.WriteLine("Not Palindrome");
+            }
+
+        }
+    }
+
